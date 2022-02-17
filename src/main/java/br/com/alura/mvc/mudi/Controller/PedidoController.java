5de6467d4d51d07ac5c2c3f80b7ff0d3f025d3e7
Macielbessa <isaacbessa02@gmail.com>
2022-02-17 15:33:54 -0300

    "Adicionando dto, controllers e adc banco de dados"

--- src/main/java/br/com/alura/mvc/mudi/Controller/PedidoController.java
+++ src/main/java/br/com/alura/mvc/mudi/Controller/PedidoController.java
@@ -1,4 +1,29 @@
 package br.com.alura.mvc.mudi.Controller;
 
+import br.com.alura.mvc.mudi.Dto.RequisicaoNovoPedido;
+import br.com.alura.mvc.mudi.Model.Pedido;
+import br.com.alura.mvc.mudi.Repository.PedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/pedido")
 public class PedidoController {
+    @Autowired
+    private PedidoRepository pedidoRepository;
+
+    @GetMapping("/formulario")
+    public String formulario(){
+        return "pedido/formulario";
+    }
+
+    @PostMapping("/novo")
+    public String novo(RequisicaoNovoPedido requisicao){
+        Pedido pedido = requisicao.toPedido();
+        pedidoRepository.save(pedido);
+        return "pedido/formulario";
+    }
 }
